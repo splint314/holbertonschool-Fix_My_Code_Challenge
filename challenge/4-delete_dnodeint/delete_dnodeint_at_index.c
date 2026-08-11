57365338e12a05d32d6a1ceaab3b560d8219fd51
splint314 <krigal323@gmail.com>
2026-08-11 14:24:12 +0200

    Fix deletion in doubly linked list

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -3,7 +3,6 @@
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
  * @head: A pointer to the first element of a list
  * @index: The index of the node to delete
  *
@@ -11,44 +10,33 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+	dlistint_t *current;
+	unsigned int i;
 
-	if (*head == NULL)
-	{
+	if (head == NULL || *head == NULL)
 		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
+
+	current = *head;
+	i = 0;
+
+	while (current != NULL && i < index)
 	{
-		*head = (*head)->next;
-		p++;
+		current = current->next;
+		i++;
 	}
-	if (p != index)
-	{
-		*head = saved_head;
+
+	if (current == NULL)
 		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
+
+	if (current->prev != NULL)
+		current->prev->next = current->next;
 	else
-	{
-		tmp = (*head)->prev;
-		tmp->next = (*head)->next;
-		if ((*head)->next)
-			(*head)->next->prev = tmp;
-		free(*head);
-		*head = saved_head;
-	}
+		*head = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
+	free(current);
+
 	return (1);
 }
